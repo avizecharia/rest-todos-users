d81c634215cd189816ca4885a87b4132b73daaa9
aviel <aviel469@gmail.com>
2024-09-30 11:04:12 +0300

    fromter(pratier)

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
-import express ,{Express} from 'express'
-import cookieParser  from 'cookie-parser'
-import 'dotenv/config'
+import express, { Express } from "express";
+import cookieParser from "cookie-parser";
+import "dotenv/config";
 
-const app:Express = express()
+const app: Express = express();
 
-app.use(express.json()) // to get the req.body
+app.use(express.json()); // to get the req.body
+app.use(cookieParser()); // to get the req.cookie(s)
 
-
-app.use(cookieParser()) // to get the req.cookie(s)
-
-
-app.listen(process.env.PORT,()=>console.log(`server is up and running , fell free to visit on http://localhost:${process.env.PORT}`))
+app.listen(process.env.PORT, () =>
+  console.log(
+    `server is up and running , fell free to visit on http://localhost:${process.env.PORT}`
+  )
+);
